a81cb368769847a26e2d7670fe8d25e24a384e83
sql3t0 <dj.sqleto@gmail.com>
2017-10-27 10:17:20 -0300

    Update solve_B0mb.sh

--- tools/solve_B0mb.sh
+++ tools/solve_B0mb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for n1 in $(seq 999);do if ./$1 <<< $(echo $n1) | grep segundo ;then for n2 in $(seq 999);do if ./$1 <<< $(echo $n1 && echo $n2) | grep terceiro;then for n3 in $(seq 999);do if ./$1 <<< $(echo $n1 && echo $n2 && echo $n3) | grep shellter;then echo "[+] $n1 | $n2 | $n3";break;fi;done;break;fi;done;break;fi;done
+for n1 in $(seq 999);do if ./$1 <<< $(echo $n1) | grep second ;then for n2 in $(seq 999);do if ./$1 <<< $(echo $n1 && echo $n2) | grep third;then for n3 in $(seq 999);do if ./$1 <<< $(echo $n1 && echo $n2 && echo $n3) | grep shellter;then echo "[+] $n1 | $n2 | $n3";break;fi;done;break;fi;done;break;fi;done
