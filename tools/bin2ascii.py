e5ac60c9bf500e838ed2949cfff7825250fcc591
sql3t0 <dj.sqleto@gmail.com>
2018-02-23 13:38:50 -0300

    Update bin2ascii.py

--- tools/bin2ascii.py
+++ tools/bin2ascii.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python
 import sys
 
-if len(sys.argv) == 1:
-    text = raw_input("Please enter binary data: ")
+if len(sys.argv) > 1:
+    text = str(sys.argv[1]) #raw_input("Please enter binary data: ")
     chars = text.split()
+    str_ = ""
+    for code in chars:
+    	code = int(code, 2)
+    	char = chr(code)
+    	str_ += char
+    
+    print(str_)
 else:
-    chars = sys.argv[1:]
-
-str_ = ""
-
-for code in chars:
-    code = int(code, 2)
-    char = chr(code)
-    str_ += char
-print(str_)
+    #chars = sys.argv[1:]
+    print('Usage : bin2ascii "binary_text"')
+    
