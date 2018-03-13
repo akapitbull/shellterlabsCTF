5f6084781d75367b0724fccc3bd36ed44ce6a865
sql3t0 <dj.sqleto@gmail.com>
2018-03-13 10:14:08 -0300

    Update bin2ascii.py

--- tools/bin2ascii.py
+++ tools/bin2ascii.py
@@ -2,17 +2,18 @@
 # chmod +x bin2ascii.py && mov bin2ascii.py bin2ascii && mv bin2ascii /usr/bin
 import sys
 
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
 if len(sys.argv) > 1:
-    text = str(sys.argv[1]) #raw_input("Please enter binary data: ")
-    chars = text.split()
+    text = str(sys.argv[1]).replace(' ','')
     str_ = ""
-    for code in chars:
-    	code = int(code, 2)
-    	char = chr(code)
-    	str_ += char
-    
+    for code in chunkstring(text,8): 
+        code = int(code, 2)
+        char = chr(code)
+        str_ += char
+    	
     print(str_)
 else:
-    #chars = sys.argv[1:]
     print('Usage : bin2ascii "binary_text"')
     
