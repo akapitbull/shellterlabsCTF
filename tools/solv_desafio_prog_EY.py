0fddc3e067ffbf3e8775bce307c9bad7b0cf85d7
sql3t0 <dj.sqleto@gmail.com>
2018-03-21 13:46:35 -0300

    Add files via upload

---
+++ tools/solv_desafio_prog_EY.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#By Sql3t0
+from pwn import *
+import sy
+
+sr = remote('158.69.192.239', 1337)
+print r.recv(2048)
+r.send("start")
+print r.recv()
+
+while True:	
+	rec = r.recv()
+	if "EY{" in rec:
+		print rec
+		break
+	
+	print rec
+	m = 0	
+	for n in eval(rec):
+		if n > m:
+			m = n	
+	
+	print '[+]Resposta : '+str(m)	
+	r.send(str(m))
