6917db79c1d0ca15e95f8c4b769f574a85a0c07e
sql3t0 <dj.sqleto@gmail.com>
2018-08-18 10:41:23 -0300

    Update ascii++.py

--- tools/ascii++.py
+++ tools/ascii++.py
@@ -1,34 +1,48 @@
+#vguvauetkrv
 import sys
 
-#ErorainZWI[kYehVcT
-msg=sys.argv[1]
-
-r=''
-for i in range(len(msg)):
-	r+= chr(ord(msg[i])+i)
-print '  : '+r
-
-r=''
-for i in range(len(msg)):
-	r+= chr(ord(msg[i])-i)
-print '  : '+r
-
-#GuswgpvcaTgxgtxgug
-for i in range(255):
+def ascii_pp(msg):
 	r=''
-	for x in sys.argv[1]:
-		try:
-			r += chr(ord(x)+i)
-		except Exception as e:
-			r += chr(ord(x)+i - 255)
-			
-	print str(i)+' : '+r
+	for i in range(len(msg)):
+		r+= chr(ord(msg[i])+i)
+	print r
 
 	r=''
-	for x in sys.argv[1]:
-		try:
-			r += chr(ord(x)-i)
-		except Exception as e:
-			r += chr(ord(x)-i + 255)
-			
-	print str(i)+' : '+r
+	for i in range(len(msg)):
+		r+= chr(ord(msg[i])-i)
+	print r
+
+	for i in range(255):
+		r=''
+		for x in msg:
+			try:
+				r += chr(ord(x)+i)
+			except Exception as e:
+				r += chr(ord(x)+i - 255)
+				
+		print r
+
+		r=''
+		for x in msg:
+			try:
+				r += chr(ord(x)-i)
+			except Exception as e:
+				r += chr(ord(x)-i + 255)
+				
+		print r
+
+if len(sys.argv) <= 1 :
+	if not sys.stdin.isatty():
+		data = sys.stdin.read()
+		print ascii_pp(data.replace('\n',''))
+	else:
+		print('Usage : scriptname.py "string_sequency"')
+		print('      : or ')
+		print('      : STDOUT | scriptname.py ')
+else:
+	if not sys.stdin.isatty():
+		data = sys.stdin.read()
+	else:
+		data = sys.argv[1]
+	
+	print ascii_pp(data.replace('\n',''))
