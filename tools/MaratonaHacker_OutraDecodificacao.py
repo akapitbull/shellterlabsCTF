18747a9e19d6cb94f1422310c8d3b1190e7866bb
sql3t0 <dj.sqleto@gmail.com>
2018-08-21 17:02:51 -0300

    Update MaratonaHacker_OutraDecodificacao.py

--- tools/MaratonaHacker_OutraDecodificacao.py
+++ tools/MaratonaHacker_OutraDecodificacao.py
@@ -37,19 +37,20 @@ import sys
 
 #txt = 6d486163107101114123052083067073073063115104111106063122063116543353066064083051083125
 txt = ['6d','48','61','63','107','101','114','123','052','083','067','073','073','063','115','104','111','106','063','122','063','116','54','33','53','066','064','083','051','083','125']
-print "[hex] (dec) {oct}\n"
+print "(dec) [hex] {oct}\n"
 for l in txt:
 	dec = ''
-	try:
-		dec += ' [%s] '%l.decode('HEX')
-	except Exception as e:	
-		dec += ' [ ] '
-		sys.stdout.write('\r')
-	
+
 	try:
 		dec += '  (%s) '%chr(int(l))
 	except Exception as e:
 		dec += '  ( ) '
+		sys.stdout.write('\r')
+
+	try:
+		dec += ' [%s] '%l.decode('HEX')
+	except Exception as e:	
+		dec += ' [ ] '
 		sys.stdout.write('\r')
 
 	try:
